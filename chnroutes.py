5aded5f9b1d4b220cc05e34ec36dcd1ac2c0e67c
qubic <qubicllj@gmail.com>
2026-02-04 15:23:04 +0800

    Add support for importing local ip data file

--- chnroutes.py
+++ chnroutes.py
@@ -7,18 +7,16 @@ import argparse
 import math
 import textwrap
 
-def generate_ovpn(metric):
-    results = fetch_ip_data()
+def generate_ovpn(metric, ipdata):
     rfile=open('routes.txt','w')
-    for ip,mask,_ in results:
+    for ip,mask,_ in ipdata:
         route_item="route %s %s net_gateway %d\n"%(ip,mask,metric)
         rfile.write(route_item)
     rfile.close()
     print("Usage: Append the content of the newly created routes.txt to your openvpn config file," \
-          " and also add 'max-routes %d', which takes a line, to the head of the file." % (len(results)+20))
+          " and also add 'max-routes %d', which takes a line, to the head of the file." % (len(ipdata)+20))
 
-def generate_linux_iproute2(metric):
-    results = fetch_ip_data()
+def generate_linux_iproute2(metric, ipdata):
     up_template = textwrap.dedent("""\
     #!/bin/bash
     export PATH="/bin:/sbin:/usr/sbin:/usr/bin"
@@ -42,23 +40,22 @@ def generate_linux_iproute2(metric):
     up_rules = ""
     down_rules = ""
 
-    for ip,mask,mask2 in results:
+    for ip,mask,mask2 in ipdata:
         up_rules += 'route add %s/%s via $OLDGW\n' % (ip,mask2)
         down_rules += 'route del %s/%s \n' % (ip,mask2)
 
     up_content = up_template.format(rules = up_rules)
     down_content = down_template.format(rules = down_rules)
 
-    upfile=open('iproute2-ip-up.sh','w')
-    downfile=open('iproute2-ip-down.sh','w')
+    upfile=open('add_chnroutes.sh','w')
+    downfile=open('remove_chnroutes.sh','w')
 
     upfile.write(up_content)
     downfile.write(down_content)
 
-    print("Copy the files whose prefix match iproute2-* to the folder /usr/local/sbin/ ")
+    print("Copy the newly created add_chnroutes.sh and remove_chnroutes.sh to the folder /usr/local/sbin/ ")
 
 def generate_linux(metric):
-    results = fetch_ip_data()
     upscript_header=textwrap.dedent("""\
     #!/bin/bash
     export PATH="/bin:/sbin:/usr/sbin:/usr/bin"
@@ -91,7 +88,7 @@ def generate_linux(metric):
     downfile.write(downscript_header)
     downfile.write('\n')
 
-    for ip,mask,_ in results:
+    for ip,mask,_ in ipdata:
         upfile.write('route add -net %s netmask %s gw $OLDGW\n'%(ip,mask))
         downfile.write('route del -net %s netmask %s\n'%(ip,mask))
 
@@ -101,9 +98,7 @@ def generate_linux(metric):
     print("For pptp only, please copy the file ip-pre-up to the folder/etc/ppp," \
           "and copy the file ip-down to the folder /etc/ppp/ip-down.d.")
 
-def generate_mac(metric):
-    results=fetch_ip_data()
-
+def generate_mac(metric, ipdata):
     upscript_header=textwrap.dedent("""\
     #!/bin/sh
     export PATH="/bin:/sbin:/usr/sbin:/usr/bin"
@@ -144,7 +139,7 @@ def generate_mac(metric):
     downfile.write(downscript_header)
     downfile.write('\n')
 
-    for ip,_,mask in results:
+    for ip,_,mask in ipdata:
         upfile.write('route add %s/%s "${OLDGW}"\n'%(ip,mask))
         downfile.write('route delete %s/%s ${OLDGW}\n'%(ip,mask))
 
@@ -155,9 +150,7 @@ def generate_mac(metric):
     print("For pptp on mac only, please copy ip-up and ip-down to the /etc/ppp folder," \
           "don't forget to make them executable with the chmod command.")
 
-def generate_android(metric):
-    results = fetch_ip_data()
-
+def generate_android(metric, ipdata):
     upscript_header=textwrap.dedent("""\
     #!/bin/sh
     alias nestat='/system/xbin/busybox netstat'
@@ -183,7 +176,7 @@ def generate_android(metric):
     downfile.write(downscript_header)
     downfile.write('\n')
 
-    for ip,mask,_ in results:
+    for ip,mask,_ in ipdata:
         upfile.write('route add -net %s netmask %s gw $OLDGW\n'%(ip,mask))
         downfile.write('route del -net %s netmask %s\n'%(ip,mask))
 
@@ -193,15 +186,32 @@ def generate_android(metric):
     print("Old school way to call up/down script from openvpn client. " \
           "use the regular openvpn 2.1 method to add routes if it's possible")
 
-
-def fetch_ip_data():
+def download_ip_data():
     #fetch data from apnic
     print("Fetching data from apnic.net, it might take a few minutes, please wait...")
     url=r'https://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest'
 
     http = urllib3.PoolManager()
     response = http.request("GET", url)
-    data=response.data.decode('utf-8')
+    return response.data.decode('utf-8')
+
+def read_ip_data(source):
+    try:
+        with open(source, 'rb') as f:
+            bytes_content = f.read() # Reads as a bytes object
+            return bytes_content.decode('utf-8') # Decodes bytes to a string
+    except FileNotFoundError:
+        print(f"Error: The file '{source}' was not found.")
+    except UnicodeDecodeError:
+        print(f"Error: Could not decode the file using UTF-8. The file might be encoded differently.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def fetch_ip_data(source):
+    if (source == "url"):
+        data=download_ip_data()
+    else:
+        data=read_ip_data(source)
 
     cnregex=re.compile(r'apnic\|cn\|ipv4\|[0-9\.]+\|[0-9]+\|[0-9]+\|a.*',re.IGNORECASE)
     cndata=cnregex.findall(data)
@@ -248,19 +258,26 @@ if __name__=='__main__':
                         nargs='?',
                         type=int,
                         help="Metric setting for the route rules")
+    parser.add_argument('-s','--source',
+                        dest='source',
+                        default='url',
+                        nargs='?',
+                        help="Ip data source")
 
     args = parser.parse_args()
 
+    ipdata = fetch_ip_data(args.source)
+
     if args.platform.lower() == 'openvpn':
-        generate_ovpn(args.metric)
+        generate_ovpn(args.metric, ipdata)
     elif args.platform.lower() == 'linux-iproute2':
-        generate_linux_iproute2(args.metric)
+        generate_linux_iproute2(args.metric, ipdata)
     elif args.platform.lower() == 'linux':
-        generate_linux(args.metric)
+        generate_linux(args.metric, ipdata)
     elif args.platform.lower() == 'mac' or args.platform.lower() == 'darwin':
-        generate_mac(args.metric)
+        generate_mac(args.metric, ipdata)
     elif args.platform.lower() == 'android':
-        generate_android(args.metric)
+        generate_android(args.metric, ipdata)
     else:
         print("Platform %s is not supported."%args.platform, file=sys.stderr)
         exit(1)
